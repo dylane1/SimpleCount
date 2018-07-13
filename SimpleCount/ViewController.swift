32e314da482d16ae18c6b708316108a157d28e45
Dylan <dylan@dylanedwards.net>
2018-07-13 12:52:53 +0800

    Initial Commit

---
+++ SimpleCount/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCount
+//
+//  Created by Dylan Edwards on 7/13/18.
+//  Copyright © 2018 Slinging Pixels Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
